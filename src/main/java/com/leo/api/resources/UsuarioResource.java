a0b03c9bdeade5fec336ba58462e83ede913aa3b
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 20:26:17 -0300

    Adicionado listagem de todos os usuários

--- src/main/java/com/leo/api/resources/UsuarioResource.java
+++ src/main/java/com/leo/api/resources/UsuarioResource.java
@@ -1,5 +1,7 @@
 package com.leo.api.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,5 +27,10 @@ public class UsuarioResource {
 		
 		
 	}
-
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
