522434a83bb2965ac0bf493d1f9b7aab50e582d3
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 13:22:00 -0300

    Instanciação dos usuários

--- src/main/java/com/leo/api/MyapiApplication.java
+++ src/main/java/com/leo/api/MyapiApplication.java
@@ -1,13 +1,22 @@
 package com.leo.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leo.api.domain.Usuario;
+
 @SpringBootApplication
-public class MyapiApplication {
+public class MyapiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario u1 = new Usuario(null, "Leonardo Wanderley" , "leo", "123");
+		Usuario u2 = new Usuario(null, "Tyrael" , "tyrael", "123");
 	}
 
 }
