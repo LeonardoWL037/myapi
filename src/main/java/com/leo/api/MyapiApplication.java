ce9af77166827cf986694a259b2b99b1b12b44eb
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 13:54:03 -0300

    ajuste application.properties e efetivação de cadastro no h2

--- src/main/java/com/leo/api/MyapiApplication.java
+++ src/main/java/com/leo/api/MyapiApplication.java
@@ -1,22 +1,36 @@
 package com.leo.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.leo.api.domain.Usuario;
+import com.leo.api.repositories.UsuarioRepository;
 
 @SpringBootApplication
 public class MyapiApplication implements CommandLineRunner{
-
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyapiApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Usuario u1 = new Usuario(null, "Leonardo Wanderley" , "leo", "123");
-		Usuario u2 = new Usuario(null, "Tyrael" , "tyrael", "123");
+		
+		Usuario u1 = new Usuario(null, "Leonardo Wanderley", "leo", "123456789");
+		Usuario u2 = new Usuario(null, "Tyrael" , "tyrael", "123456789");
+	
+		
+		usuarioRepository.saveAll(Arrays.asList(u1, u2));
 	}
-
+	
+	
+	
+	
 }
