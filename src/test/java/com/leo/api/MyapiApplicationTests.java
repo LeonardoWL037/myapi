0eb065c2b0a01ba2bab92e0fa66c7904a63ddf00
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 12:54:30 -0300

    Primeiro commit

---
+++ src/test/java/com/leo/api/MyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
